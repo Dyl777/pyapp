ddb52cc1003d9f652765b4ee4aa43c8b8f56cf1c
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2024-10-31 15:26:51 +0100

    Update test.py
    Test for testing

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify 
+from flask import Flask, jsonify
+import unittest from pytest
 
 app = Flask(__name__)
 
@@ -6,5 +7,17 @@ app = Flask(__name__)
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance")
 
+class TestMyApp(unitTest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.app.get('/')
+        self.assertEqual(response.json, {'message': 'Hello level 400 FET, Quality Assurance!'})
+
+
+
 if __name__ == '__main__':
    app.run(debug=True)
+   unittest.main()
