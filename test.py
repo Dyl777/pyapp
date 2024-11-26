637879efc553bbb33ab75fe64dcc25467f01a714
Dyl777 <117218643+Dyl777@users.noreply.github.com>
2024-11-26 18:58:36 +0100

    Final test.py
    Updated

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-import unittest
 
 app = Flask(__name__)
 
@@ -7,19 +6,7 @@ app = Flask(__name__)
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
-class TestMyApp(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_home_route(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)  # Check the response status code
-        self.assertEqual(response.json, {'message': 'Hello level 400 FET, Quality Assurance!'})
 
 if __name__ == '__main__':
     # Run the Flask app only if not running tests
-    import sys
-    if 'unittest' in sys.modules:
-        unittest.main()
-    else:
-        app.run(debug=True)
+    app.run(debug=True)
